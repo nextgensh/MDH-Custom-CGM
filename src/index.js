53318226c4f23ab1fa5e78751ef02a4534d6fca5
Shravan Aras <123.shravan@gmail.com>
2022-11-23 17:58:11 -0700

    Custom view for CGM
    Initial design and implementation of the custom homework view for CGM project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom'
+import Homework from './views/Homework'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// Special case testing with the local environment variable.
+if (window.location.hostname === 'localhost') {
+  window.MyDataHelps.setParticipantAccessToken({
+    'access_token': process.env.REACT_APP_ACCESS_TOKEN,
+    'expires_in': 21600,
+    'token_type': 'Bearer'
+  }, 'https://mydatahelps.org/')
+}
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <Router>
+      <Routes>
+        <Route path='/' element={<Homework/>} />
+      </Routes>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
---
+++ src/views/Homework.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import MyDataHelps from '@careevolution/mydatahelps-js'
+import { Card, NavigationBar, Action, Layout, StatusBarBackground, ProjectHeader } from '@careevolution/mydatahelps-ui'
+
+export default function() {
+    function homeworkAction() {
+        MyDataHelps.getParticipantInfo().then((result) => {
+            let linkIdentifier = result.linkIdentifier
+            let surveyURL = 'https://mydatahelps.org/mydatahelps/'+linkIdentifier+'/surveylink/?surveyName=Food%20Survey'
+
+            window.location.href = surveyURL
+        })
+
+    }
+
+    return (
+        <Layout>
+            <StatusBarBackground />
+            <ProjectHeader />
+            <Card>
+                <Action 
+                    title='Glucose Tracking Homework'
+                    subtitle='Tap here to start your homework'
+                    onClick={homeworkAction}
+                />
+            </Card>
+        </Layout>
+    )
+}
